f8b5694b91133e312831f7771640d7313ad20155
kapil upadhyay <87381213+kapi1upadhyay@users.noreply.github.com>
2023-07-10 22:40:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+  
+    // TODO: Implement login functionality
+  }
+  
+  function createUser() {
+    // TODO: Implement create user functionality
+  }
+  
+  function editUser() {
+    // TODO: Implement edit user functionality
+  }
+  
+  function deleteUser() {
+    // TODO: Implement delete user functionality
+  }
+  
+  function createBlogPost() {
+    // TODO: Implement create blog post functionality
+  }
+  
+  window.onload = function() {
+    // TODO: Initialize the page
+  };
+  
